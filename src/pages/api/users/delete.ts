351b600875a0e9bb1ae828fbf4873c26930dcf3b
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-11-30 02:44:26 +0530

    DELETE

--- src/pages/api/users/delete.ts
+++ src/pages/api/users/delete.ts
@@ -13,7 +13,9 @@ export default async function handler(
     }
 
     try {
-      await query<unknown>("DELETE FROM users WHERE id = $1", [id]);
+      await query<{ message?: string }>("DELETE FROM users WHERE id = $1", [
+        id,
+      ]);
       res.status(200).json({ message: "User deleted successfully" });
     } catch (error: unknown) {
       if (error instanceof Error) {
